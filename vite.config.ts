d74f62c244f8904b7b39a30f1bd66a59e589288b
Sanya200405 <133080368+Sanya200405@users.noreply.github.com>
2026-08-05 11:20:30 +0530

    changes from 3rd id

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Website/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 });
